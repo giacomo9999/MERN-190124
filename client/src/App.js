9e13ba79d927e7447ae8643bfbb5cb54636c6a1e
James Gary <gmarchesi888@gmail.com>
2019-01-24 15:03:43 -0500

    Added axios, other setup stuff.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
+    const pStyle = {
+      fontSize: "30px",
+      textAlign: "center"
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 style={pStyle}>I'M READY TO USE THE BACK END APIS</h1>
       </div>
     );
   }
